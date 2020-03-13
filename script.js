7453c881ae62e8c98fd4d1d77cc3ed9b7662b2df
Bridget Asser <bridgetasser@gmail.com>
2020-03-13 00:25:22 -0400

    check guessed number and more logic

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { write } from "fs";
-
 const msgEl = document.getElementById("msg");
 
 const randNum = getRandomNumber();
@@ -15,9 +13,37 @@ recognition.start();
 
 function onSpeak(e) {
     const msg = e.results[0][0].transcript;
-    console.log(e);
+    // console.log(e);
     writeMessage(msg);
     checkNumber(msg);
+}
+
+function writeMessage(msg) {
+    msgEl.innerHTML = `
+    <div>You said:</div>
+    <span class="box">${msg}</span> 
+    `;
+}
+
+function checkNumber(msg) {
+    const num = +msg;
+    if (Number.isNaN(num)) {
+        msgEl.innerHTML += `<div>That aint no number Ive eva seen</div>`;
+        return;
+    }
+    if (num > 100 || num < 1) {
+        msgEl.innerHTML = `<div>Number must be between 1 and 100</div>`;
+        return;
+    }
+    if (num === randNum) {
+        document.body.innerHTML = `<h1> Niceeee..that was the one <br></br> It was ${num}!</h2>
+        <button class="play-again" id="play-again">Play Again </button>
+        `;
+    } else if (num > randNum) {
+        msgEl.innerHTML += `<div> Try less</div>`;
+    } else {
+        msgEl.innerHTML += `<div> Aim for more</div>`;
+    }
 }
 
 function getRandomNumber() {
@@ -25,3 +51,11 @@ function getRandomNumber() {
 }
 
 recognition.addEventListener("result", onSpeak);
+
+recognition.addEventListener("end", () => recognition.start());
+
+document.body.addEventListener("click", e => {
+    if (e.target.id == "play-again") {
+        window.location.reload();
+    }
+});
